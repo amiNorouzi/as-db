6d7eab5e5b8e8db3992c2ddfc97e49e0113090bf
Amin <aminnorouzi99@gmail.com>
2023-04-25 21:12:32 +0330

    3.sql completed

--- sql/3.sql
+++ sql/3.sql
@@ -1,8 +1,9 @@
+#? for
 DELIMITER //
 
 CREATE PROCEDURE insert_fake_data()
 BEGIN
-    FOR i IN 1..1000000 DO
+    FOR i IN 1..1000 DO
     insert into products (sku, name, price, weight, description, thumbnail, image, category, created_at, stock)
     values (concat('DGK123', i), 'S21', 1000, 210, 'Samsung', 'phone1.png', 'phone1.png', 'phone', created_at, 100);
 END FOR;
@@ -12,4 +13,41 @@ DELIMITER ;
 
 CALL insert_fake_data();
 
-select * from products
+# ? loop
+DELIMITER //
+
+CREATE PROCEDURE insert_fake_data_with_loop()
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    loop_over_data: LOOP
+        IF i > 100 THEN
+            LEAVE loop_over_data;
+        END IF;
+        insert into products (sku, name, price, weight, description, thumbnail, image, category, created_at, stock)
+        values (concat('DGK123', i), 'S21', 1000, 210, 'Samsung', 'phone1.png', 'phone1.png', 'phone', created_at, 100);
+        SELECT i;
+        SET i = i + 1;
+    END LOOP;
+END //
+
+DELIMITER ;
+
+CALL insert_fake_data_with_loop();
+
+#? while
+DELIMITER //
+
+CREATE PROCEDURE insert_fake_data_with_while()
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    WHILE i <= 1000
+        DO
+            insert into products (sku, name, price, weight, description, thumbnail, image, category, created_at, stock)
+            values (concat('DGK123', i), 'S21', 1000, 210, 'Samsung', 'phone1.png', 'phone1.png', 'phone', created_at,100);
+            SELECT i;
+            SET i = i + 1;
+        END WHILE;
+END //
+
+DELIMITER ;
+CALL insert_fake_data_with_while();
