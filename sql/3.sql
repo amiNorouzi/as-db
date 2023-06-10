09ad3357d44fa01ad2fe1dc0416159e8b07b08e4
Amin <aminnorouzi99@gmail.com>
2023-06-10 13:11:40 +0330

    6.sql

--- sql/3.sql
+++ sql/3.sql
@@ -55,27 +55,3 @@ END //
 
 DELIMITER ;
 CALL insert_fake_data_with_while();
-
-#? recursive
-DELIMITER //
-
-CREATE PROCEDURE recursive_insert_fake_data(IN i INT, OUT result INT)
-recursive_loop:
-BEGIN
-    SET max_sp_recursion_depth = 255;
-    IF i > 30 THEN
-        set result = 0;
-        LEAVE recursive_loop;
-    END IF;
-    insert into products (sku, name, price, weight, description, thumbnail, image, category, created_at, stock)
-    values (concat('DGK123', i), 'S21', 1000, 210, 'Samsung', 'phone1.png', 'phone1.png', 'phone', created_at, 100);
-    SELECT i;
-    SET i = i + 1;
-    CALL recursive_insert_fake_data(i, result);
-END //
-
-DELIMITER ;
-
-SET @i = 0;
-SET @result = 0;
-CALL recursive_insert_fake_data(@i, @result);
---
+++ sql/7.sql
@@ -0,0 +1,19 @@
+SELECT *
+FROM products
+WHERE name LIKE '%پفک%';
+
+ALTER TABLE products
+    ADD FULLTEXT (name);
+SELECT *
+FROM products
+WHERE MATCH (name) AGAINST ('+پفک' IN NATURAL LANGUAGE MODE);
+
+SELECT *
+FROM products
+WHERE MATCH (name) AGAINST ('+پفک')
+ORDER BY MATCH (name) AGAINST ('+پفک') DESC;
+
+SELECT name, MATCH (name) AGAINST ('+پفک +پنیر -نمک *' IN NATURAL LANGUAGE MODE) AS score
+FROM products
+WHERE MATCH (name) AGAINST ('+پفک +پنیر -نمک *' IN NATURAL LANGUAGE MODE)
+ORDER BY score DESC;
