e6f2101d0931de89f011f3480f567125c678b212
Amin <aminnorouzi99@gmail.com>
2023-06-10 13:21:45 +0330

    refactored 4.sql

--- sql/4.sql
+++ sql/4.sql
@@ -1,16 +1,13 @@
-create definer = root@localhost view product_view as
-select `az_db`.`products`.`id`        AS `id`,
-       `az_db`.`products`.`name`      AS `name`,
-       `az_db`.`products`.`price`     AS `price`,
-       `az_db`.`products`.`thumbnail` AS `thumbnail`,
-       `az_db`.`products`.`stock`     AS `stock`
-from `az_db`.`products`;
+create view product_view as
+select id, name, price, thumbnail, stock
+from products;
 
-create definer = root@localhost view product_cat_view as
-select `az_db`.`products`.`id`        AS `product_id`,
-       `az_db`.`products`.`name`        AS `pname`,
-       `az_db`.`categories`.`id`        AS `category_id`,
-       `az_db`.`products`.`thumbnail`   AS `pthumbnail`,
-       `az_db`.`categories`.`thumbnail` AS `cthumbnail`,
-       `az_db`.`categories`.`name`      AS `cname`
-from (`az_db`.`categories` join `az_db`.`products`) where az_db.products.id between 104 and 120;
+create view product_cat_view as
+select products.id          AS product_id,
+       products.name        AS pname,
+       categories.id        AS category_id,
+       products.thumbnail   AS pthumbnail,
+       categories.thumbnail AS cthumbnail,
+       categories.name      AS cname
+from (categories join products)
+where products.id between 104 and 120;
